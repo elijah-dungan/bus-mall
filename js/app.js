c5b5f82d3660ba76bb6ea453986e72e7b0937288
root <eli.dungan@gmail.com>
2019-06-07 05:21:32 -0700

    adjusted animation

--- js/app.js
+++ js/app.js
@@ -91,6 +91,7 @@ function render() { // renders images and radio buttons while assigning their ap
 function defaultStyle() {
   imgOneEl.style.boxShadow = '0 12px 5px rgb(0, 0, 0, 0.23)';
   imgOneEl.style.margin = '-5px 0 0 0';
+  imgOneEl.style.filter = 'brightness(100%)';
 }
 
 function renderVotes() {
@@ -113,23 +114,29 @@ function changeHandler(e) {
     for(var j = 0; j < options.length; j ++) {
       if(options[0].checked) {
         imgOneEl.style.boxShadow = '0 12px 5px rgb(0, 0, 0, 0.23)';
+        imgOneEl.style.filter = 'brightness(100%)';
         imgOneEl.style.margin = '-5px 0 0 0';
       } else {
         imgOneEl.style.boxShadow = '0 6px 4px rgb(0, 0, 0, 0.40)';
         imgOneEl.style.margin = '0 0 -5px 0';
+        imgOneEl.style.filter = 'brightness(90%)';
       }
       if(options[1].checked) {
         imgTwoEl.style.boxShadow = '0 12px 5px rgb(0, 0, 0, 0.23)';
+        imgTwoEl.style.filter = 'brightness(100%)';
         imgTwoEl.style.margin = '-5px 0 0 0';
       } else {
         imgTwoEl.style.boxShadow = '0 6px 4px rgb(0, 0, 0, 0.40)';
+        imgTwoEl.style.filter = 'brightness(90%)';
         imgTwoEl.style.margin = '0 0 -5px 0';
       }
       if(options[2].checked) {
         imgThreeEl.style.boxShadow = '0 12px 5px rgb(0, 0, 0, 0.23)';
+        imgThreeEl.style.filter = 'brightness(100%)';
         imgThreeEl.style.margin = '-5px 0 0 0';
       } else {
         imgThreeEl.style.boxShadow = '0 6px 4px rgb(0, 0, 0, 0.40)';
+        imgThreeEl.style.filter = 'brightness(90%)';
         imgThreeEl.style.margin = '0 0 -5px 0';
       }
     }
