6cd0e8389803c2af9e48c685f1834791eb713856
root <eli.dungan@gmail.com>
2019-06-09 16:46:11 -0700

    conversion to bar chart

--- js/app.js
+++ js/app.js
@@ -130,49 +130,19 @@ function renderVotes() {
   console.log(votes);
   var ctx = document.getElementById('myChart').getContext('2d');
   var myChart = new Chart(ctx, {
-    type: 'line',
+    type: 'bar',
     data: {
       labels: names,
       datasets: [{
-        label: 'Number of Votes',
-        data: votes,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
+        label: 'Number of Views',
+        data: views,
+        backgroundColor: 'rgb(54, 162, 235)',
         borderWidth: 1
       },
       {
-        label: 'Number of Views',
-        data: views,
-        backgroundColor: [
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-          'rgba(255, 99, 132, 0.2)'
-        ],
-        borderColor: [
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-          'rgba(255, 99, 132, 1)'
-        ],
+        label: 'Number of Votes',
+        data: votes,
+        backgroundColor: 'rgb(255, 99, 132)',
         borderWidth: 1
       }
     ]
